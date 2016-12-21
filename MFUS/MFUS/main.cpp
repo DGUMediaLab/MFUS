b55a534b492bc0eeee28f0f0bfd5ab2fd38d44f8
JonHa Lee <nairoon@naver.com>
2016-12-21 19:27:53 +0900

    base form
    .

---
+++ MFUS/MFUS/main.cpp
@@ -0,0 +1,10 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+
+int main(){
+	/**/printf("OpenCV Version : %s\n\n", CV_VERSION);
+
+
+	return 0;
+}
