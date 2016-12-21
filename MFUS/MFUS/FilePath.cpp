bb7802327a38ef08f9d2078e70a8c964246a5db7
JonHa Lee <nairoon@naver.com>
2016-12-21 19:30:14 +0900

    FilePath 관리 클래스 생성
    .

---
+++ MFUS/MFUS/FilePath.cpp
@@ -0,0 +1,20 @@
+#include "FilePath.h"
+
+FilePath* FilePath::instance_ = nullptr;
+
+FilePath* FilePath::getInstance(){
+	if (instance_ == nullptr)
+		instance_ = new FilePath();
+
+	return instance_;
+}
+
+const std::string FilePath::getColorPath(int number){
+
+	return filepath + dataName + std::string("\\Color\\") + std::string("KinectScreenshot_RGB") + std::to_string(number) + std::string(".bmp");
+}
+
+const std::string FilePath::getDepthPath(int number){
+
+	return filepath + dataName + std::string("\\Depth\\") + std::string("Filedepth_") + std::to_string(number) + std::string(".bin");
+}
