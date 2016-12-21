8f0dbbfea564f1794f02cd6d04db7cb36496ef0a
JonHa Lee <nairoon@naver.com>
2016-12-21 21:20:04 +0900

    3.2 구현
    findContours를 통해 모든 contour를 구한다.

--- MFUS/MFUS/main.cpp
+++ MFUS/MFUS/main.cpp
@@ -11,6 +11,7 @@
 #include "BinaryStream.h"
 
 
+#include <time.h>
 
 
 //Data 읽어오는 메소드들
@@ -30,8 +31,6 @@ int main(){
 	/*
 	3.1 Problem Formulation
 	current time t = kFrameNumber
-	I = colorImage
-	D = depthImage
 	x = inital_segmentation	
 	*/		
 
@@ -45,7 +44,46 @@ int main(){
 	//각 단계 마다 불 필요한 메모리 삭제
 	delete bodyIndexData;
 
+	/*
+	3.2 Foreground Hole Detection
+	*/
 
+	//initial Segmentation 영상 만들기, 검정색이 배경
+	cv::Mat img_initial_segmentation(COLOR_HEIGHT, COLOR_WIDTH, CV_8UC1, cv::Scalar(0));
+	for (int row = 0; row < COLOR_HEIGHT; row++){
+		for (int col = 0; col < COLOR_WIDTH; col++){
+
+			if (initial_segmentation[row * COLOR_WIDTH + col] == 1){
+				img_initial_segmentation.at<uchar>(row, col) = 255;
+			}
+		}
+	}
+	//cv::imshow("Initial Segmentation", img_initial_segmentation);
+
+
+	//Initial binary image로부터 모든 contours 구하기
+	//외각선 배열. 즉, 논문에서 요구하는 모든 contours
+	vector<vector<cv::Point>> contours; 
+	//외각선들 간의 계층구조
+	vector<cv::Vec4i> hierarchy;		
+	//CV_RETR_TREE = contour 검색 결과를 tree 구조로 저장. 이 때 바깥 쪽에 있을 수록 루트
+	cv::findContours(img_initial_segmentation, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE); 
+
+	int nAreaCount = contours.size();
+	printf("contour의 개수 = %d\n", nAreaCount);
+
+	//contours 영상 만들기	
+	/* 
+	srand(time(NULL));
+	cv::Mat output(COLOR_HEIGHT, COLOR_WIDTH, CV_8UC3, cv::Scalar(0, 0, 0));
+	for (int i = 0; i < nAreaCount; i++){
+		cv::Scalar color = cv::Scalar(rand() % 255, rand() % 255, rand() % 255);
+		int thickness = 2;
+		cv::drawContours(output, contours, i, color, thickness, 8, hierarchy);
+	}
+	cv::imshow("Contours", output);
+	*/
+	
 	//이후 단계 구현
 
 
